cfb407bbb8e07d098c5527283e88cfbbc6f84aa0
ansango <anibalsantosgo@gmail.com>
2021-08-19 22:23:44 +0200

    containers

---
+++ src/components/ui/view/ContainerTheme.js
@@ -0,0 +1,7 @@
+export const ContainerTheme = ({ children }) => {
+  return (
+    <div className="bg-white dark:bg-gray-900 text-blue-900 dark:text-blue-200 ">
+      {children}
+    </div>
+  );
+};
